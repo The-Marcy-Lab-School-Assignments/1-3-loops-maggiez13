bbcea51366772e47cd9afab94b0b7be84fdcbd40
Mei Zhu <maggiez0613@gmail.com>
2024-04-08 00:53:14 -0400

    up to q7 done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,18 +1,43 @@
 const loop0UpTo10 = () => {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
 };
 
+// loop0UpTo10();
+
 const loop5to10 = () => {
+  for (let i = 5; i <= 10; i++) {
+    console.log(i);
+  }
 };
+
+// loop5to10();
 
 // you do NOT need an if check
 const loopEvenNumbersUpTo10 = () => {
+  for (let i = 0; i < 10; i += 2) {
+    console.log(i);
+  }
 };
+
+// loopEvenNumbersUpTo10();
 
 const countdown5to0 = () => {
+  for (let i = 5; i >= 0; i--) {
+    console.log(i);
+  }
 };
 
-const loopUpToNum = () => {
+// countdown5to0();
+
+const loopUpToNum = (num) => {
+  for (let i = 0; i < num; i++) {
+    console.log(i);
+  }
 };
+
+// loopUpToNum(5);
 
 const fizzbuzz = () => {
 };
--- src/modify.js
+++ src/modify.js
@@ -1,19 +1,41 @@
 const whileToFor = () => {
-  let i = 0;
-  while (i < 5) {
+  for (let i = 0; i < 5; i++) {
     console.log(i);
-    i++;
   }
 };
 
+whileToFor()
+
+
+// const whileToFor = () => {
+//   let i = 0;
+//   while (i < 5) {
+//     console.log(i);
+//     i++;
+//   }
+// };
+
+
+
 const continueGuardClause = () => {
   for (let i = 1; i < 5; i++) {
-    if (!(i === 2) && !(i === 3)) {
-      console.log("Sure glad this isn't 2 or 3");
-      console.log(i);
+    if ((i === 2) && (i === 3)) {
+      continue;
     }
+    // if condition is true, continue will skip rest of code in block and go to next iteration
+    console.log("Sure glad this isn't 2 or 3");
+    console.log(i);
   }
 };
+
+// const continueGuardClause = () => {
+//   for (let i = 1; i < 5; i++) {
+//     if (!(i === 2) && !(i === 3)) {
+//       console.log("Sure glad this isn't 2 or 3");
+//       console.log(i);
+//     }
+//   }
+// };
 
 module.exports = {
   whileToFor,
